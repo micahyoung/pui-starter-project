c88c688c27c83149c797a53125461bee93e81abc
Micah Young <myoung@pivotal.io>
2016-04-13 16:53:56 -0400

    Bug in tooltips in a modal dialog
    Demonstrates an issue with tooltips in modal dialogs, where tooltips
    are not position=right and pin={false}.
    
    Signed-off-by: Rasheed Abdul-Aziz <squeedee@gmail.com>

--- js/myComponents.js
+++ js/myComponents.js
@@ -1,24 +1,71 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-const Banner = require('pui-react-ribbons').Banner;
-
-// Bootstrap Javascript is required for some PUI CSS components, such as accordians, collapse, dropdowns,
-// modals, and tooltips
-// require('bootstrap');
-
-// Required modules here like so:
-// var DefaultButton = require('pui-react-buttons').DefaultButton;
-
+var BaseModal = require('pui-react-modals').BaseModal;
+var ModalBody = require('pui-react-modals').ModalBody;
+var ModalFooter = require('pui-react-modals').ModalFooter;
+var DefaultButton = require('pui-react-buttons').DefaultButton;
+var Tooltip = require('pui-react-tooltip').Tooltip;
+var OverlayTrigger = require('pui-react-overlay-trigger').OverlayTrigger;
 
 var MyTestPage = React.createClass({
+  getInitialState: function() {
+    return {
+      modalOpen: false
+    };
+  },
+  _openModal: function() {
+    this.setState({modalOpen: true});
+  },
+
+  _closeModal: function() {
+    this.setState({modalOpen: false});
+  },
+
   render: function() {
     return (
-      // Put your react components here, like so:
-      // <DefaultButton>
-      //   I'm a button to click!
-      // </DefaultButton>
-
-    <Banner>Your React Components Go Here</Banner>
+      <div>
+        <DefaultButton onClick={this._openModal}>
+          Open Stateless Modal
+        </DefaultButton>
+        <BaseModal title='What a Header!'
+                   className='optional-custom-class'
+                   show={this.state.modalOpen}
+                   onHide={this._closeModal}>
+          <ModalBody>
+            <div>
+              <p>
+                Check out this
+                <OverlayTrigger placement="left" pin={false} overlay={<Tooltip>I should be on the left</Tooltip>}>
+                  <span className="overlay-trigger" tabIndex="0"> tooltip on the left.</span>
+                </OverlayTrigger>
+              </p>
+              <p>
+                Check out this
+                <OverlayTrigger placement="right" pin={false} overlay={<Tooltip>I should be on the right</Tooltip>}>
+                  <span className="overlay-trigger" tabIndex="0"> tooltip on the right.</span>
+                </OverlayTrigger>
+              </p>
+              <p>
+                Check out this
+                <OverlayTrigger placement="top" pin={false} overlay={<Tooltip>I should be on the top</Tooltip>}>
+                  <span className="overlay-trigger" tabIndex="0"> tooltip on the top.</span>
+                </OverlayTrigger>
+              </p>
+              <p>
+                Check out this
+                <OverlayTrigger placement="bottom" pin={false} overlay={<Tooltip>I should be on the bottom</Tooltip>}>
+                  <span className="overlay-trigger" tabIndex="0"> tooltip on the bottom.</span>
+                </OverlayTrigger>
+              </p>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <DefaultButton onClick={this._closeModal}>
+              Close
+            </DefaultButton>
+          </ModalFooter>
+        </BaseModal>
+      </div>
     );
   }
 });
